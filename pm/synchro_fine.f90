b61a65494fa1a63ad502680e6978f82996d5a2f3
Cristian <cristian.g.barrera@durham.ac.uk>
2018-12-04 17:33:33 +0000

    safety brackets for condition

--- pm/synchro_fine.f90
+++ pm/synchro_fine.f90
@@ -502,7 +502,7 @@ subroutine sync(ind_grid,ind_part,ind_grid_part,ng,np,ilevel)
      if(gr.and.gr_newtonian.and.gr2) dteff(j)=-dteff(j) ! Revert Newtonian synchro
   end do
 
-  if(.not.gr.and..not.gr_newtonian)then
+  if((.not.gr).and.(.not.gr_newtonian))then
      ! Update particles level
      do j=1,np
         levelp(ind_part(j))=ilevel
