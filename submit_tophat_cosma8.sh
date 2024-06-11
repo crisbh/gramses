4470e48cb703410a35d591a6dccfe606f60ed6e9
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2024-06-11 13:55:15 +0100

    added snapshot backing up to submit script

--- submit_tophat_cosma8.sh
+++ submit_tophat_cosma8.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH -n 256
+#SBATCH -n 128
 #SBATCH -t 02:00:00
 #SBATCH -J tophat
 #SBATCH -o ./logs/%j.log
@@ -20,8 +20,24 @@ unset I_MPI_HYDRA_BOOTSTRAP
 cd bin && make clean && make && cd ..
 
 # run
-#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_B512_PM256.nml
-mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM256_cosma8.nml
+# smooth density field with parts at cell boundaries
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM64_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_B256_PM512_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM64_test_cosma8.nml
+mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_B256_PM128_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM128_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM256_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM256_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_PM512_cosma8.nml
+#mpirun -np $SLURM_NTASKS ./bin/ramses3d ./namelist/gr_tophat_B1243_PM256_cosma8.nml
+
+# Back up simulation data
+echo "Job done. Now backing up data..."
+JOB_DIR_NAME="data_job_"$SLURM_JOBID
+mkdir $JOB_DIR_NAME
+cp -r output_000* $JOB_DIR_NAME
+cp logs/$SLURM_JOBID.* $JOB_DIR_NAME
+
 
 # report
 echo ""
